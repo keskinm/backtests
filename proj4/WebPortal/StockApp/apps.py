07ed3d5c236fbbb4e3b133046fc58014026426fe
Sasha <mouss42490@gmail.com>
2022-10-23 15:17:09 +0200

    Delete proj4

--- proj4/WebPortal/StockApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StockappConfig(AppConfig):
-    name = 'StockApp'
--- proj4/WebPortal/StockApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('', views.homepage, name='home'),
-	path('fileupload/', views.fileUpload, name='fileupload'),
-	path('ajax/checkprogress/', views.checkProgress, name='checkprogress'),
-]
