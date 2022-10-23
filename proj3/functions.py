89b7de58b9f62118477ca485df200a809d03b1cd
Sasha <mouss42490@gmail.com>
2022-10-23 16:20:33 +0200

    Some refactoring.

--- proj3/functions.py
+++ proj3/functions.py
@@ -1,12 +1,14 @@
 import numpy as np
 import math
 
+
 # prints formatted price
-def formatPrice(n):
+def format_price(n):
 	return ("-$" if n < 0 else "$") + "{0:.2f}".format(abs(n))
 
+
 # returns the vector containing stock data from a fixed file
-def getStockDataVec(key):
+def get_stock_data_vec(key):
 	vec = []
 	lines = open("data/" + key + ".csv", "r").read().splitlines()
 
@@ -15,16 +17,18 @@ def getStockDataVec(key):
 
 	return vec
 
+
 # returns the sigmoid
 def sigmoid(x):
 	return 1 / (1 + math.exp(-x))
 
+
 # returns an an n-day state representation ending at time t
-def getState(data, t, n):
+def get_state(data, t, n):
 	d = t - n + 1
 	block = data[d:t + 1] if d >= 0 else -d * [data[0]] + data[0:t + 1] # pad with t0
 	res = []
-	for i in xrange(n - 1):
+	for i in range(n - 1):
 		res.append(sigmoid(block[i + 1] - block[i]))
 
 	return np.array([res])
